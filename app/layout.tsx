b7dd9412e71e2b3ecb77a9ce00554ae583a3c1f5
Isracraxcker <andyisraeljaramillo@gmail.com>
2026-04-17 16:23:03 -0500

    fix: sync lockfile

--- app/layout.tsx
+++ app/layout.tsx
@@ -68,19 +68,19 @@ export const metadata: Metadata = {
   icons: {
     icon: [
       {
-        url: "/icon-light-32x32.png",
+        url: "https://img.icons8.com/ios-filled/50/mando.png",
         media: "(prefers-color-scheme: light)",
       },
       {
-        url: "/icon-dark-32x32.png",
+        url: "https://img.icons8.com/ios-filled/50/mando.png",
         media: "(prefers-color-scheme: dark)",
       },
       {
-        url: "/icon.svg",
+        url: "https://img.icons8.com/ios-filled/50/mando.png",
         type: "image/svg+xml",
       },
     ],
-    apple: "/apple-icon.png",
+    apple: "https://img.icons8.com/ios-filled/50/mando.png",
   },
 };
 
